5c6a9eb23cc58cd6e7df8c7689f7b25960c3438e
Stephen Roberts <sjrob.net@gmail.com>
2023-03-01 10:11:58 +0000

    small file location changes

--- MSC/New_format/Code/run_over_files.sh
+++ MSC/New_format/Code/run_over_files.sh
@@ -1,2 +1,2 @@
 #for file in $1; do tail -n +2 $file | cut -d',' -f1,2,4,8- ; done > test.csv
-for file in $1; do tail -n +2 $file ; done > ../../../../Temp/test.csv
+for file in $1; do tail -n +2 $file ; done > ~/Temp/test.csv
