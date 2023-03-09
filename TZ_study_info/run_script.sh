e5c0a2e9b199f85e5e48a0540f3ce9df0e11689f
Stephen Roberts <sjrob.net@gmail.com>
2023-03-09 11:34:36 +0000

    slight changes to .sh

--- TZ_study_info/run_script.sh
+++ TZ_study_info/run_script.sh
@@ -1,1 +1,1 @@
-python mosq_numbers_from_csv.py test.csv > out.csv & ; cat house_mosq_uuid.csv | grep -i $1 > test.csv
+python mosq_numbers_from_csv.py test.csv > out.csv & ; cat house_mosq_uuid.csv > test.csv
