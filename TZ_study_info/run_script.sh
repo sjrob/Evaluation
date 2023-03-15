c5d7f7a2f107d8c8e617baff4b98f324b6cf6969
Stephen Roberts <sjrob.net@gmail.com>
2023-03-15 14:06:49 +0000

    small changes to .sh

--- TZ_study_info/run_script.sh
+++ TZ_study_info/run_script.sh
@@ -1,3 +1,3 @@
 mkfifo test.csv
-python mosq_numbers_from_csv.py test.csv > out.csv & ; cat $1 > test.csv
+python mosq_numbers_from_csv.py test.csv > $2 & ; cat $1 > test.csv
 rm test.csv
--- TZ_study_info/run_script_with_grep_filtering.sh
+++ TZ_study_info/run_script_with_grep_filtering.sh
@@ -1,3 +1,3 @@
 mkfifo test.csv
-python mosq_numbers_from_csv.py test.csv > out.csv & ; cat $1 | grep -i $2 > test.csv
+python mosq_numbers_from_csv.py test.csv > $3 & ; cat $1 | grep -i $2 > test.csv
 rm test.csv
